e448cb5a1503f1f599e1e02601a94aa2e7cded45
Jack Willis <jack@attac.us>
2018-06-06 00:21:08 -0500

    Move PDF generation responsibility into bin/cpp.rs

--- bin/cpp.rs
+++ bin/cpp.rs
@@ -1,10 +1,13 @@
 extern crate clap;
-
 extern crate lopdf;
 extern crate num_rational;
 extern crate pdf_extract;
+extern crate tempdir;
 
 extern crate font_metrics;
+
+use std::fs::File;
+use std::io::Write;
 
 use num_rational::Ratio;
 
@@ -15,25 +18,48 @@ fn main() {
         .about("Calculates the characters per pica (cpp) of specially formatted PDF file.")
         .author("https://github.com/jackwillis/font-metrics/")
         .arg(
-            clap::Arg::with_name("FILE")
-                .help("The PDF file to analyze")
-                .required(true)
-                .index(1),
+            clap::Arg::with_name("font")
+                .short("f")
+                .long("font")
+                .help("Name of font to test")
+                .takes_value(true)
+                .required(true),
         )
         .arg(
             clap::Arg::with_name("picas")
                 .short("w")
                 .long("width")
-                .help("Number of picas per line in the PDF file")
-                .required(true)
-                .takes_value(true),
+                .help("Number of picas per line")
+                .takes_value(true)
+                .default_value("32"),
         )
         .get_matches();
 
-    let filename = matches.value_of("FILE").unwrap();
+    let font_name = matches.value_of("font").unwrap();
     let picas_per_line = matches.value_of("picas").unwrap().parse::<i32>().unwrap();
 
-    let document = lopdf::Document::load(filename).unwrap();
+    let working_dir = tempdir::TempDir::new("cpp").unwrap();
+
+    let latex_file_path = working_dir.path().join("cpp.tex");
+
+    let mut latex_file = File::create(&latex_file_path).unwrap();
+    let source = generate_latex_source(font_name, 12, 36);
+    latex_file.write_all(source.as_bytes()).unwrap();
+
+    let mut xelatex = std::process::Command::new("xelatex");
+    let command = xelatex
+        .current_dir(&working_dir)
+        .arg("-quiet")
+        .arg("-interaction=nonstopmode")
+        .arg(latex_file_path.to_str().unwrap().to_owned());
+
+    println!("{:?}", &command);
+
+    command.status().unwrap();
+
+    let pdf_file_path = working_dir.path().join("cpp.pdf");
+
+    let document = lopdf::Document::load(pdf_file_path).unwrap();
     let text = extract_text(document);
 
     let cpp = chars_per_line(text) / picas_per_line;
@@ -43,6 +69,8 @@ fn main() {
         cpp,
         ratio_into_f32(cpp).unwrap()
     );
+
+    working_dir.close().unwrap();
 }
 
 fn chars_per_line(text: String) -> Ratio<i32> {
@@ -54,6 +82,25 @@ fn chars_per_line(text: String) -> Ratio<i32> {
         .fold(0, |sum, line| sum + line.len());
 
     Ratio::new(total_chars as i32, lines.len() as i32)
+}
+
+fn generate_latex_source(font_name: &str, font_size: i32, text_width: i32) -> String {
+    format!(
+        r"
+\documentclass[{font_size}pt]{{article}}
+\usepackage[textwidth = {text_width}pc]{{geometry}}
+\usepackage{{fontspec, microtype, blindtext}}
+\pagestyle{{empty}}
+\setmainfont{{{font_name}}}[OpticalSize = 0]
+\begin{{document}}
+\noindent
+\blindtext
+\end{{document}}
+    ",
+        font_name = font_name,
+        font_size = font_size,
+        text_width = text_width
+    )
 }
 
 fn extract_text(document: lopdf::Document) -> String {
--- bin/xheight.rs
+++ bin/xheight.rs
@@ -1,31 +1,26 @@
 extern crate clap;
 
-extern crate num_rational;
-
 extern crate font_metrics;
 
-use font_metrics::ratio_into_f32;
-
 fn main() {
-    let app = clap::App::new("xheight")
+    let matches = clap::App::new("xheight")
         .about("Calculates the x-height/cap height ratio of a TrueType font.")
         .author("https://github.com/jackwillis/font-metrics/")
         .arg(
-            clap::Arg::with_name("FILE")
-                .help("Sets the input file to use")
+            clap::Arg::with_name("FILENAME")
+                .help("The location of the TrueType font to measure")
                 .required(true)
                 .index(1),
-        );
+        )
+        .get_matches();
 
-    let matches = app.get_matches();
-    let filename = matches.value_of("FILE").unwrap();
-
+    let filename = matches.value_of("FILENAME").unwrap();
     let font = font_metrics::read_font_from_filename(filename);
     let ratio = font_metrics::x_height_ratio(&font);
 
     println!(
         "x-height ratio: {} (~{:.3})",
         ratio,
-        ratio_into_f32(ratio).unwrap()
+        font_metrics::ratio_into_f32(ratio).unwrap()
     );
 }
